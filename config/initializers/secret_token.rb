ae0eaf005dd7bd4b91a74d32115caa7be331961f
cunhafernando <cunhafernandofj@gmail.com>
2014-03-24 20:10:50 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SinasempuRjOrgBr::Application.config.secret_token = 'eb0891f17ee3bf76799e73a0e15da41b66f6178ff10f1e20093e5162ac0c90d82ad4e3b0f8e368a54c46cee9de5e3c93680224266f41afd86bdb250bbf8492b8'
